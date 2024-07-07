3c45e234fce2e9dd6745f160b52c5a85108c4fae
Matheus Bernardes <matheusbnas@gmail.com>
2024-07-06 23:16:29 -0300

    Feat: Mudar link

--- 1_🏠_home.py
+++ 1_🏠_home.py
@@ -10,7 +10,7 @@ if "data" not in st.session_state:
     st.session_state["data"] = df_data
 
 st.markdown("# FIFA23 OFFICIAL DATASET! ⚽️")
-st.sidebar.markdown("Desenvolvido por [Asimov Academy](https://asimov.academy)")
+st.sidebar.markdown("Desenvolvido por [Matech 3D](https://matech3d.com.br/)")
 
 
 btn = st.link_button(
